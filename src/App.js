844745f72dc9f45c1d7f3bf271861b9e26d0475e
ralphZero <placideralph@gmail.com>
2021-01-29 01:00:04 -0500

    changes browserRouter to HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Index from './components/screens/Index';
 import JobPage from './components/screens/JobPage';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import JobContextProvider from './contexts/JobContext';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <JobContextProvider>
           <Route exact path='/' component={Index} />
           <Route path='/:id' component={JobPage} /> 
         </JobContextProvider>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
